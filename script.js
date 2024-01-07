a467c20ca2d10d354194ac063dc64fee022f2eaa
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2024-01-07 17:02:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const para = document.getElementById("counter
+const btn = document.getElementById("incrementBtn");
+btn.addEventListener("click", function()) {
+	let count=Number(para.innerText);
+	alert(count);
+	para.innerText = ++count;
+}
+
+
